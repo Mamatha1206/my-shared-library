b55173db32042d9586a1a29c0c747c4f645b0bbe
Mamatha Eudulakanti <119730722+Mamatha1206@users.noreply.github.com>
2025-05-03 14:20:32 +0530

    Create HelloWorld.java

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,8 @@
+// my-shared-library/src/main/java/com/example/HelloWorld.java
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
